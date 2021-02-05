b62e20deb4f76df057514507c3b9e1011150cc44
Henry <henry96guaman@gmail.com>
2021-02-05 16:54:18 -0500

    creacion de menu principal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 //autenticacion para logout
 import { AuthService } from "../servicios/auth.service";
-
+import { MenuController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Observable } from 'rxjs';
 
 
@@ -12,8 +15,14 @@ import { Observable } from 'rxjs';
 })
 export class HomePage {
 
-  constructor(public authservice: AuthService,) { }
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    public authservice: AuthService
+  ) {  }
 
+  
   Onlogout() {
     this.authservice.logout();
   }
