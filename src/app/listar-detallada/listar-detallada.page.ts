d3967f504e1d2ea7095d08992ee95a3df0ab3260
pulpo1993 <marcelo_1993@hotmail.es>
2021-02-15 21:13:03 -0500

    crud solucionado y completo

--- src/app/listar-detallada/listar-detallada.page.ts
+++ src/app/listar-detallada/listar-detallada.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { ListaRecordatoriosService } from '../services/lista-recordatorios.service';
-
+import {RecordatorioserviceService} from '../services/recordatorioservice.service';
+import { Recordatorio } from '../model/recordatorio';
 @Component({
   selector: 'app-listar-detallada',
   templateUrl: './listar-detallada.page.html',
@@ -9,22 +10,41 @@ import { ListaRecordatoriosService } from '../services/lista-recordatorios.servi
 })
 export class ListarDetalladaPage implements OnInit {
 
+  actividadId;
+  actividad;
   recor:any[];
 
   constructor(
     private ObtenerRecordatorio: ListaRecordatoriosService,
-    private route: ActivatedRoute, private router: Router,
+    private route: ActivatedRoute, private router: Router,private recordatorioServicio:RecordatorioserviceService,
   ) { }
 
   ngOnInit() {
    
-    
+    this.cargarDetalleActividad()
   }
   volver(){
-    
-
-    this.router.navigate(['/listar-detallada']);
+    this.router.navigate(['/listar-recordatorios']);
   }
+
+  async cargarDetalleActividad(){
+    await this.route.params.subscribe(params =>{
+      this.actividadId=params.uid;
+    });
+    
+    await this.recordatorioServicio.getRecordatorioById(this.actividadId).subscribe(
+      res =>{
+        console.log("esto llega")
+        console.log(res)
+        this.actividad =res.docs[0].data() as Recordatorio
+      }
+
+    )
+    
+  }
+
+  
+
   
 
 }
